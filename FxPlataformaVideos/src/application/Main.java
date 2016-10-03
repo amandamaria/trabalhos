a15efee301b40941ac5d42dcd39873c45cbf9f2b
amandamaria <amanda.marry@hotmail.com>
2016-10-02 21:56:05 -0300

    Teste de commit

--- FxPlataformaVideos/src/application/Main.java
+++ FxPlataformaVideos/src/application/Main.java
@@ -1,15 +1,17 @@
 package application;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+	
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			//Teste commit
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
